10e01df2a9bdb201ee35e7964a3fe3ee168306ae
PhicLabs <128798010+PhicLabs@users.noreply.github.com>
2023-10-10 11:15:11 +0800

    Update pharos.sol

--- pharos.sol
+++ pharos.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.1/token/ERC20/ERC20.sol";
 
-contract Pharo is ERC20 {
-    constructor() ERC20("Pharo", "PHA") {
+contract Pharos is ERC20 {
+    constructor() ERC20("Pharos", "PHA") {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
 }
